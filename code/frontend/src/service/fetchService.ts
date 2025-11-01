ccf68e4dff588b4e12d20abd62c4ab3d40fe0d4e
InteractiveNinja <gabrielnadolny2@gmail.com>
2025-11-01 11:19:11 +0100

    adds endpoint enum

--- code/frontend/src/service/fetchService.ts
+++ code/frontend/src/service/fetchService.ts
@@ -1,9 +1,11 @@
 // TODO: dont hardcode the API url :/
+import {Endpoints} from "./Endpoints";
+
 const API_ENDPOINT = `http://localhost:4200/api`;
 
 
-export function loginUser(username: string) {
-    return fetch(`${API_ENDPOINT}/login`, {
+export function loginUser(username: string): Promise<void> {
+    return fetch(`${API_ENDPOINT}/${Endpoints.LOGIN}`, {
         method: 'POST',
         body: JSON.stringify({username})
     }).then(res => res.ok ? Promise.resolve() : Promise.reject())
