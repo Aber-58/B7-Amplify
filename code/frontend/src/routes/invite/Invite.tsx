51c228d032f17e98ba98cda32495db45aea22234
AnnaMaria2711 <anna-maria.buelk@coop.ch>
2025-11-01 13:11:58 +0100

    add styling to invite page

--- code/frontend/src/routes/invite/Invite.tsx
+++ code/frontend/src/routes/invite/Invite.tsx
@@ -1,21 +1,22 @@
-import {useParams} from "react-router";
+import { useParams } from "react-router";
 import QRCode from "react-qr-code";
+import "./Invite.css";
 
 function Invite() {
-    let { uuid } = useParams();
+    const { uuid } = useParams();
 
     function createInviteUrl() {
         return `${window.location.host}/join/${uuid}`;
     }
-    return <>
-        <h1>Invite</h1>
 
-        <QRCode
-            size={20}
-            style={{ height: "auto", maxWidth: "100%", width: "25%" }}
-            value={createInviteUrl()}
-        />
-    </>
+    return (
+        <div className="invite-container">
+            <h1>Invite</h1>
+            <div className="qr-wrapper">
+                <QRCode value={createInviteUrl()} />
+            </div>
+        </div>
+    );
 }
 
-export default Invite
+export default Invite;
