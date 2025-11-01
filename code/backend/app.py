85d7348b5e27a9da26c6705bbcd80d1ba34da18b
Daniel Breuer <echo ue.rbnad@liam | rev>
2025-11-01 03:30:05 +0100

    pages scaffolding

--- code/backend/app.py
+++ code/backend/app.py
@@ -1,6 +1,8 @@
 from flask import Flask
+from pages import routes
 
 app = Flask(__name__)
+app.register_blueprint(routes)
 
 @app.route('/')
 def home():
---
+++ code/backend/pages.py
@@ -0,0 +1,29 @@
+from flask import Blueprint
+import uuid
+
+routes = Blueprint('routes', __name__)
+
+@routes.route('/admin')
+def admin():
+    return "admin dashboard. create qr codes for topics here"
+
+@routes.route('/invite/<uuid_param>')
+def invite(uuid_param):
+    return "display qr code here"
+
+@routes.route('/join/<uuid_param>')
+def join(uuid_param):
+    return "where qr code points to"
+
+@routes.route('/login')
+def login():
+    return "page to enter username (html form?). give user a session id"
+
+@routes.route('/poll/<uuid_param>')
+def poll(uuid_param):
+    return "give opinion on topic"
+
+@routes.route('/live/<uuid_param>')
+def live(uuid_param):
+    return "live room with cool clusters of opinions (and leader chat after clusters have been formed)"
+
