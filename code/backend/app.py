8ec0486b4e814f9facfe541e33b3cdf849a721a7
InteractiveNinja <gabrielnadolny2@gmail.com>
2025-11-01 08:10:37 +0100

    prefixes endpoints with '/api' and adds status endpoint

--- code/backend/app.py
+++ code/backend/app.py
@@ -1,12 +1,20 @@
-from flask import Flask
+import os
+from flask import Flask, send_from_directory
 from pages import routes
+from flask_cors import CORS
 
-app = Flask(__name__)
-app.register_blueprint(routes)
+app = Flask(__name__, static_folder='../frontend/build', static_url_path='')
+app.register_blueprint(routes, url_prefix='/api')
+CORS(app)
 
-@app.route('/')
-def home():
-    return "Hello, Flask!"
+@app.route('/', defaults={'path': ''})
+@app.route('/<path:path>')
+def serve(path):
+    if path != "" and os.path.exists(app.static_folder + '/' + path):
+        return send_from_directory(app.static_folder, path)
+    else:
+        return send_from_directory(app.static_folder, 'index.html')
+
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000)
+    app.run(host='0.0.0.0', port=4200)
--- code/backend/pages.py
+++ code/backend/pages.py
@@ -3,27 +3,37 @@ import uuid
 
 routes = Blueprint('routes', __name__)
 
+
+@routes.route('/status')
+def status():
+    return "Ok!"
+
+
 @routes.route('/admin')
 def admin():
     return "admin dashboard. create qr codes for topics here"
+
 
 @routes.route('/invite/<uuid_param>')
 def invite(uuid_param):
     return "display qr code here"
 
+
 @routes.route('/join/<uuid_param>')
 def join(uuid_param):
     return "where qr code points to"
+
 
 @routes.route('/login')
 def login():
     return "page to enter username (html form?). give user a session id"
 
+
 @routes.route('/poll/<uuid_param>')
 def poll(uuid_param):
     return "give opinion on topic"
 
+
 @routes.route('/live/<uuid_param>')
 def live(uuid_param):
     return "live room with cool clusters of opinions (and leader chat after clusters have been formed)"
-
