0a63775348521c90384318ee38ae75d5aa1d30f5
Daniel Breuer <echo ue.rbnad@liam | rev>
2025-11-01 07:40:53 +0100

    dont spam containers

--- code/run.sh
+++ code/run.sh
@@ -3,4 +3,4 @@
 SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 
 mkdir -p "$SCRIPT_DIR/../state"
-docker run -it -p 5000:5000 -v "$SCRIPT_DIR/..":/app -v "$SCRIPT_DIR/../state":/state amplify:latest
+docker run --rm -it -p 5000:5000 -v "$SCRIPT_DIR/..":/app -v "$SCRIPT_DIR/../state":/state amplify:latest
