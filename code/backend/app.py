905960e95ce64e6abf5cc292a5a38a068bf2037e
InteractiveNinja <gabrielnadolny2@gmail.com>
2025-11-01 13:02:52 +0100

    handle errors to still redirect to react

--- code/backend/app.py
+++ code/backend/app.py
@@ -11,11 +11,14 @@ CORS(app)
 @app.route('/', defaults={'path': ''})
 @app.route('/<path:path>')
 def serve(path):
-    if path != "" and os.path.exists(app.static_folder + '/' + path):
+    file_path = os.path.join(app.static_folder, path)
+    if os.path.exists(file_path):
         return send_from_directory(app.static_folder, path)
-    else:
-        return send_from_directory(app.static_folder, 'index.html')
+    return send_from_directory(app.static_folder, 'index.html')
 
+@app.errorhandler(404)
+def not_found(e):
+    return send_from_directory(app.static_folder, 'index.html')
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=FLASK_PORT)
