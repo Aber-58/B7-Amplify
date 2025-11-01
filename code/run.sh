454b6cc00412416f550bf802652c59034c9065dc
Daniel Breuer <echo ue.rbnad@liam | rev>
2025-11-01 01:40:07 +0100

    add env, state dir, container build and run scripts

---
+++ code/run.sh
@@ -0,0 +1,6 @@
+#!/usr/bin/env bash
+
+SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
+
+mkdir -p "$SCRIPT_DIR/../state"
+docker run -it -p 5000:5000 -v "$SCRIPT_DIR/..":/app -v "$SCRIPT_DIR/../state":/state amplify:latest
