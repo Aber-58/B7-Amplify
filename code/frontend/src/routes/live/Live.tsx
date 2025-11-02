f865228291d1f74b49a2541c3ffe816fb2b7afc5
InteractiveNinja <gabrielnadolny2@gmail.com>
2025-11-02 10:35:30 +0100

    adds mock chat

--- code/frontend/src/routes/live/Live.tsx
+++ code/frontend/src/routes/live/Live.tsx
@@ -1,9 +1,10 @@
 import Opinion from "./opinion/Opinion";
-import {useEffect, useState} from "react";
+import {KeyboardEventHandler, useEffect, useState} from "react";
 import {useParams} from "react-router";
 import {getLiveClusters} from "../../service/fetchService";
 import './Live.css'
 import {LiveViewResponse} from "../../service/model/LiveViewResponse";
+import {Message} from "../../service/model/Message";
 
 
 function Live() {
@@ -13,23 +14,37 @@ function Live() {
     useEffect(() => {
         if (!uuid) return;
         getLiveClusters(uuid).then(setLiveView)
-    })
+    }, [uuid])
+
+    function sendMessage(e: React.KeyboardEvent<HTMLInputElement>) {
+        if (e.key !== 'Enter') {
+            return
+        }
+
+        if (liveView) {
+            setTextbox("")
+            let newMessage: Message = {text: textbox};
+            const extendedSortedMessages: Message[] = liveView.sortedMessages.length > 0 ? [...liveView.sortedMessages, newMessage] : [newMessage]
+            setLiveView({...liveView, sortedMessages: extendedSortedMessages})
+        }
+    }
 
 
     return <>
         <h1 className="problem-title">{liveView?.problemTitle}</h1>
         <div className="center-grid">
-            {liveView?.solutions.map((solution, index) => (<Opinion solution={solution} index={index}></Opinion>))}
+            {liveView?.solutions.map((solution, index) => (
+                <Opinion key={index} solution={solution} index={index}></Opinion>))}
         </div>
         <div className="chatbox">
             <div className="messages">
-                {liveView?.sortedMessages.map((message) => (<>
-                    <p className="message"
-                       title={message.timestamp.toString()}>{`${message.author}: ${message.text}`}</p>
+                {liveView?.sortedMessages.map((message, index) => (<>
+                    <p key={index} className="message">{message.text}</p>
                 </>))}
 
             </div>
-            <input value={textbox} onChange={e => setTextbox(e.target.value)} className="chat" placeholder="Chat"/>
+            <input value={textbox} onKeyDown={e => sendMessage(e)} onChange={e => setTextbox(e.target.value)}
+                   className="chat" placeholder="Chat"/>
         </div>
     </>
 }
